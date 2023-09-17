eabca9528fe29c12dd57a836fbf40e39a50183c4
escheive <scheiveerik@gmail.com>
2023-09-16 22:27:12 -0700

    created utils folder and added a helpers functions file with a capitalize string and format number functions for aesthetic purposes

---
+++ utils/helperFunctions.tsx
@@ -0,0 +1,41 @@
+const capitalizeString = (string) => {
+    if (typeof string === 'string') {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    } else if (typeof string === 'object' && string !== null) {
+        return string.name.charAt(0).toUpperCase() + string.name.slice(1);
+    }
+}
+
+export { capitalizeString };
+
+const formatNumber = (number: number) => {
+  if (number < 1e3) {
+    return number.toString();
+  } else if (number < 1e6) {
+    return (number / 1e3).toFixed(2) + 'k';
+  } else if (number < 1e9) {
+    return (number / 1e6).toFixed(2) + 'm';
+  } else if (number < 1e12) {
+    return (number / 1e9).toFixed(2) + 'b';
+  } else if (number < 1e15) {
+    return (number / 1e12).toFixed(2) + 't';
+  } else if (number < 1e18) {
+    return (number / 1e15).toFixed(2) + 'qa';
+  } else if (number < 1e21) {
+    return (number / 1e18).toFixed(2) + 'qu';
+  } else if (number < 1e24) {
+    return (number / 1e21).toFixed(2) + 'sx';
+  } else if (number < 1e27) {
+    return (number / 1e24).toFixed(2) + 'sp';
+  } else if (number < 1e30) {
+    return (number / 1e27).toFixed(2) + 'o';
+  } else if (number < 1e33) {
+    return (number / 1e30).toFixed(2) + 'n';
+  } else if (number < 1e36) {
+    return (number / 1e33).toFixed(2) + 'd';
+  } else {
+    return number
+  }
+}
+
+export { formatNumber };
