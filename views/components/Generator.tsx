0c23cd57c0c825e9115aecc8a09b7f87f665aba8
escheive <scheiveerik@gmail.com>
2023-09-16 22:52:07 -0700

    integrated formatNumber function with generators so that their prices are legible

--- views/components/Generator.tsx
+++ views/components/Generator.tsx
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import { useSelector } from 'react-redux';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
+import { formatNumber } from '../../utils/helperFunctions';
+
 const Generator = ({generatorKey, generatorCostFormula }) => {
     const generator = useSelector((state) => state.generatorsReducer[generatorKey]);
     const generatorCost = useMemo(() => generatorCostFormula(generator.purchasedQuantity));
@@ -10,7 +12,7 @@ const Generator = ({generatorKey, generatorCostFormula }) => {
   return (
     <View style={styles.generatorContainer}>
       <Text>{generator.name} x{generator.totalQuantity}</Text>
-      <Button onPress={() => console.log('buy generator pressed')} title={`Buy ${generator.name} \$${generatorCost}`}/>
+      <Button onPress={() => console.log('buy generator pressed')} title={`Buy ${generator.name} \$${formatNumber(generatorCost)}`}/>
     </View>
   )
 
