22df518a89ec6a2615537ac45a7db6fccebe5940
escheive <scheiveerik@gmail.com>
2023-09-23 18:48:38 -0700

    built out gameLoop hook to replace old timer

--- utils/hooks.tsx
+++ utils/hooks.tsx
@@ -6,3 +6,98 @@ import type { RootState, AppDispatch } from '../store';
 type DispatchFunc = () => AppDispatch;
 export const useAppDispatch: DispatchFunc = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// Game loop hook to track time within the game
+export const useGameLoop = (options = {}) => {
+  const [state, setState] = useState('STOPPED');
+  const frameRef = useRef(null);
+  const timing = useRef({ last: null, total: 0, delta: 0, lag: 0});
+  const numberOfUpdates = useRef(0);
+
+  const {
+    tickSpeed = 1000,
+    step = 1000/60,
+    maxUpdates = 300,
+    onUpdate = () => {},
+    onRender = () => {},
+    onPanic = () => {},
+  } = options;
+
+  const tick = (time) => {
+    if (timing.current.last === null) {
+      timing.current.last = time;
+    }
+    timing.current.delta = time - timing.current.last;
+    timing.current.total += timing.current.delta;
+    timing.current.lag += timing.current.delta;
+    timing.current.last = time;
+
+    while (timing.current.lag >= step) {
+      timing.current.lag -= step;
+      onUpdate(step, timing.current.total);
+      numberOfUpdates.current++;
+      if (numberOfUpdates.current >= maxUpdates) {
+        onPanic();
+        break;
+      }
+    }
+
+    onRender(timing.current.lag / step);
+
+    frameRef.current = requestAnimationFrame(tick);
+  }
+
+  const isStopped = state === 'STOPPED';
+  const isPaused = state === 'PAUSED';
+  const isRunning = state === 'RUNNING';
+
+  const start = () => {
+    if (isStopped) {
+      setState('RUNNING');
+
+      timing.current = { last: null, total: 0, delta: 0, lag: 0};
+      numberOfUpdates.current = 0;
+
+      frameRef.current = requestAnimationFrame(tick);
+    }
+  };
+
+  const stop = () => {
+    if (isRunning || isPaused) {
+      setState('STOPPED');
+      cancelAnimationFrame(frameRef.current);
+    }
+  };
+
+  const pause = () => {
+    if (isRunning) {
+      setState('PAUSED');
+      cancelAnimationFrame(frameRef.current);
+    }
+  };
+
+  const resume = () => {
+    if (isPaused) {
+      setState('RUNNING');
+      frameRef.current = requestAnimationFrame(tick);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (frameRef.current) {
+        cancelAnimationFrame(frameRef.current);
+      }
+    };
+  }, []);
+
+  return {
+    isStopped,
+    isPaused,
+    isRunning,
+    start,
+    stop,
+    pause,
+    resume,
+  };
+};
