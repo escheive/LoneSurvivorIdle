c3f394b3f82c7cca86ee1ae62819a3f111905705
escheive <scheiveerik@gmail.com>
2023-09-30 17:47:51 -0700

    created an offlineGainsPopup component that will show users how much they gained since they were last online

---
+++ views/components/OfflineGainsPopup.tsx
@@ -0,0 +1,48 @@
+// OfflineGainsPopup.js
+
+import React from 'react';
+import { View, Text, Modal, TouchableOpacity, StyleSheet } from 'react-native';
+
+const OfflineGainsPopup = ({ message, isVisible, onClose }) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={() => onClose()}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text>{message}</Text>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => onClose()}>
+            <Text>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 35,
+    alignItems: 'center',
+  },
+  closeButton: {
+    marginTop: 10,
+    backgroundColor: 'lightgray',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
+
+export default OfflineGainsPopup;
