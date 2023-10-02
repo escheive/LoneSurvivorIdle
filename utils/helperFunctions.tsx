0ed18564add291235f27a4f3815d4741e578acaf
escheive <scheiveerik@gmail.com>
2023-10-01 17:52:45 -0700

    added a formatTime function to utils

--- utils/helperFunctions.tsx
+++ utils/helperFunctions.tsx
@@ -44,3 +44,15 @@ export const formatNumber = (number: number) => {
   }
 };
 
+export const formatTime = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
