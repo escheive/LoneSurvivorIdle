0dc4caea82ea5f7d4bc1b1141ed9fc0fdaed5f62
escheive <scheiveerik@gmail.com>
2023-10-01 19:42:43 -0700

    added some styling to currencyHeader

--- views/components/currencies/BaseCurrencyHeader.tsx
+++ views/components/currencies/BaseCurrencyHeader.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useAppDispatch, useAppSelector } from '../../../utils/hooks';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { selectCurrency } from '../../../store/reducers/currencySlice';
 import { setCurrency } from '../../../store/reducers/currencyReducer';
@@ -13,11 +14,30 @@ const CurrencyHeader = () => {
   const currency = useAppSelector(selectCurrency);
 
   return (
-    <View>
-      <Text>${formatNumber(currency.money.toFixed(2))}</Text>
-      <Text>Prestige Points: {formatNumber(currency.prestigePoints.toFixed(2))}</Text>
-    </View>
+    <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 0}}
+        colors={['rgba(140, 62, 16, 0)', 'rgba(140, 62, 16, 0.3)', 'rgba(140, 62, 16, 0)']}
+        style={styles.currencyContainer}
+    >
+      <Text style={styles.currencyText}>${formatNumber(currency.money.toFixed(2))}</Text>
+    </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+    currencyContainer: {
+        width: '25%',
+        alignItems: 'center',
+    },
+    currencyText: {
+        width: '100%',
+        textAlign: 'center',
+        marginVertical: 1,
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
+})
 
 export default CurrencyHeader;
