829871eefaf1cf02dbcb70f9fcb5592738c1a741
escheive <scheiveerik@gmail.com>
2023-10-06 20:24:55 -0700

    added currency and salvagedTechEarned on reset display for salvage screen

--- views/screens/SalvageScreen.tsx
+++ views/screens/SalvageScreen.tsx
@@ -3,19 +3,23 @@ import { ScrollView, View, Text } from 'react-native';
 import { useAppDispatch, useAppSelector } from '../../utils/hooks';
 
 import { selectSalvage, incrementSalvageUpgrade } from '../../store/reducers/salvageSlice';
+import { selectCurrency } from '../../store/reducers/currencySlice';
 
 import SalvageUpgradeComponent from '../components/SalvageUpgradeComponent';
 import ScreenWithBackButton from '../components/ScreenWithBackButton';
 
 import { calculateSalvagedTechEarned } from '../../utils/gameLogic';
+import { formatNumber } from '../../utils/helperFunctions';
 
 const SalvageScreen = () => {
   const dispatch = useAppDispatch();
   const salvageUpgrades = useAppSelector(selectSalvage);
+  const currency = useAppSelector(selectCurrency);
 
   return (
     <ScreenWithBackButton>
-      <Text></Text>
+      <Text>{currency.prestigePoints}</Text>
+      <Text>{formatNumber(calculateSalvagedTechEarned(currency.money))}</Text>
       <ScrollView>
         {salvageUpgrades.map((upgrade) => (
           <SalvageUpgradeComponent upgrade={upgrade} />
