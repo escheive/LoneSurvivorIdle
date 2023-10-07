2e6ecee6f580293c4019f4df9bfdef5aa5df9b8e
escheive <scheiveerik@gmail.com>
2023-10-06 20:30:46 -0700

    added a perform salvage reset button and a function to handle resets that is in progress

--- views/screens/SalvageScreen.tsx
+++ views/screens/SalvageScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useAppDispatch, useAppSelector } from '../../utils/hooks';
 
 import { selectSalvage, incrementSalvageUpgrade } from '../../store/reducers/salvageSlice';
@@ -16,17 +16,61 @@ const SalvageScreen = () => {
   const salvageUpgrades = useAppSelector(selectSalvage);
   const currency = useAppSelector(selectCurrency);
 
+  const performSalvageReset = () => {
+
+  }
+
   return (
     <ScreenWithBackButton>
-      <Text>{currency.prestigePoints}</Text>
-      <Text>{formatNumber(calculateSalvagedTechEarned(currency.money))}</Text>
+      <Text style={styles.currentSalvagedTechText}>{currency.prestigePoints}</Text>
+      <Text style={styles.salvagedTechGainedOnResetText}>Salvaged Tech gained on reset: {formatNumber(calculateSalvagedTechEarned(currency.money))}</Text>
       <ScrollView>
         {salvageUpgrades.map((upgrade) => (
           <SalvageUpgradeComponent upgrade={upgrade} />
         ))}
       </ScrollView>
+      <TouchableOpacity
+        onPress={performSalvageReset}
+        style={styles.resetButton}
+      >
+        <Text style={styles.resetButtonText}>Salvage Reset</Text>
+      </TouchableOpacity>
     </ScreenWithBackButton>
   )
 }
+
+const styles = StyleSheet.create({
+  currentSalvagedTechText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  salvagedTechGainedOnResetText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  resetButton: {
+    backgroundColor: 'rgba(201, 67, 61, 0.7)',
+    padding: 10,
+    ...Platform.select({
+      android: {
+        elevation: 1,
+      },
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+      },
+    })
+  },
+  resetButtonText: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+})
 
 export default SalvageScreen;
